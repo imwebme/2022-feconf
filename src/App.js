daef148f439957900e10b25243242581f2186247
pguswls <alfma123@naver.com>
2022-10-07 14:07:31 +0900

    mobile

--- src/App.js
+++ src/App.js
@@ -1,74 +1,82 @@
-import logo from './logo.svg';
-import './style/main.css';
-import Intro from './components/Intro';
-import Event from './components/Event';
-import About from './components/About';
-import {useState, useRef} from 'react'
-import Keyword from './components/Keyword';
-import Mission from './components/Mission';
-import Engineer from './components/Engineer';
-
+import logo from "./logo.svg";
+import "./style/main.css";
+import Intro from "./components/Intro";
+import Event from "./components/Event";
+import About from "./components/About";
+import { useState, useRef } from "react";
+import Keyword from "./components/Keyword";
+import Mission from "./components/Mission";
+import Engineer from "./components/Engineer";
 
 function App() {
   const [size, setSize] = useState({});
   const imageRef = useRef(null);
-  const getSize = () => 
+  const getSize = () =>
     setSize({
       width: imageRef.current.clientWidth,
-      height: imageRef.current.clientHeight
-    })
-  
+      height: imageRef.current.clientHeight,
+    });
+
   return (
-    <>   
+    <>
       <div className="root_container">
         <div className="container">
           <header className="header">
-          <nav className="nav">
-            <div className="logoWapper">
-              <img className="header_logo"
-              src="/img/logo.png" alt="imweb-logo" ref={imageRef} 
-              onLoad={getSize}/>
-              <img className="header_logo" src="/img/feconf_logo.png"
-              alt='FECONF' 
-              ref={imageRef}
-              onLoad={getSize}/>
-            </div>
-            <ul className="menu">
-              <li className="menuItem">
-                <a href="#about">About</a>
-              </li>
-              <li className="menuItem">
-                <a href="#enginner">Imweb Engineer</a>
-              </li>
-              <li className="menuItem">
-                <a href="#event">Event</a>
-              </li>
-            </ul>
+            <nav className="nav">
+              <div className="logoWapper">
+                <div>
+                  <img
+                    className="header_logo"
+                    src="/img/logo.png"
+                    alt="imweb-logo"
+                    ref={imageRef}
+                    onLoad={getSize}
+                  />
+                </div>
+                <div>
+                  <img
+                    className="header_logo"
+                    src="/img/feconf_logo.png"
+                    alt="FECONF"
+                    ref={imageRef}
+                    onLoad={getSize}
+                  />
+                </div>
+              </div>
+              <ul className="menu">
+                <li className="menuItem">
+                  <a href="#about">About</a>
+                </li>
+                <li className="menuItem">
+                  <a href="#enginner">Imweb Engineer</a>
+                </li>
+                <li className="menuItem">
+                  <a href="#event">Event</a>
+                </li>
+              </ul>
             </nav>
           </header>
-        <main className="main">
-          <Intro onLoad={getSize}></Intro>
-          <About onLoad={getSize}></About>
-          <Keyword onLoad={getSize}></Keyword>
-          <Mission></Mission>
-          <Engineer></Engineer>
-          <Event onLoad={getSize}></Event>
-        </main>
-        <footer className="footer">
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <span className="logo">
-            </span>
-          </a>
-        </footer>
+          <div className="main">
+            <Intro onLoad={getSize}></Intro>
+            <About onLoad={getSize}></About>
+            <Keyword onLoad={getSize}></Keyword>
+            <Mission></Mission>
+            <Engineer></Engineer>
+            <Event onLoad={getSize}></Event>
+          </div>
+          <footer className="footer">
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by <span className="logo"></span>
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,59 +1,81 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-const Intro = ({getSize}) => {
+const Intro = ({ getSize }) => {
   const [size, setSize] = useState({});
   const imageRef = useRef(null);
 
   return (
     <>
+      <div className="intro-container talking-section">
+        <div className="intro-image-container" style={{ display: "flex" }}>
+          <div className="intro-image-wrap" style={{ flex: "40%" }}>
+            <img
+              className="intro-image"
+              src="/img/typing_2.gif"
+              ref={imageRef}
+            ></img>
+          </div>
 
-    <div className='intro-container talking-section' >
-    <div className='intro-image-container'style={{display: 'flex'}}>
-     <div className='intro-image-wrap' >
-     <img className='intro-image'
-      src="/img/texting.png"
-      ref={imageRef}
-      >
-      </img>
-      <img className='intro-image-2'
-      src="/img/typing_3.gif"></img>
+          <div className="fake-image" style={{ flex: "60%" }}></div>
+        </div>
+        <div>
+          <img
+            className="intro-image"
+            src="/img/hello1.png"
+            ref={imageRef}
+          ></img>
+          <img
+            className="intro-image"
+            src="/img/hello2.png"
+            ref={imageRef}
+          ></img>
+        </div>
+        <div className="intro-image-container" style={{ display: "flex" }}>
+          <div className="intro-image-wrap" style={{ flex: "40%" }}>
+            <img
+              className="intro-image"
+              src="/img/typing_2.gif"
+              ref={imageRef}
+            ></img>
+          </div>
+          <div className="fake-image" style={{ flex: "60%" }}></div>
+        </div>
+        <img
+          className="intro-image"
+          src="/img/message1.png"
+          ref={imageRef}
+        ></img>
       </div>
-      <div className='fake-image' style={{width : '50%'}}></div>
+      <div className="n-container hidden-tablet">
+        <div className="intro-container product-section">
+          <img
+            className="intro-image"
+            src="/img/apple.png"
+            ref={imageRef}
+          ></img>
+          <img className="intro-image" src="/img/star.png" ref={imageRef}></img>
+        </div>
       </div>
-      <img className='intro-image'
-      src="/img/hello1.png"
-      ref={imageRef}
-      ></img>
-      <img className='intro-image'
-      src="/img/hello2.png"
-      ref={imageRef}
-      ></img>
-   <div className='intro-image-wrap'>
-     <img className='intro-image'
-      src="/img/texting.png"
-      ref={imageRef}
-      >
-      </img>
-      <img className='intro-image-2'
-      src="/img/typing_3.gif"></img>
-      </div>
-      <img className='intro-image'
-      src="/img/message1.png"
-      ref={imageRef}
-      ></img>
-    </div>
-    <div className='intro-container product-section'>
-      <img className='intro-image'
-      src="/img/apple.png"
-      ref={imageRef}
-      ></img>
-    <img className='intro-image'
-      src="/img/star.png"
-      ref={imageRef}
-      ></img>
+      <div className="n-container hidden-pc">
+        <div className="intro-container product-section">
+          <div className="apple-wrap">
+            <img
+              className="intro-image"
+              src="/img/apple.png"
+              ref={imageRef}
+            ></img>
+          </div>
+          <div className="star-wrap">
+            <img
+              className="intro-image"
+              src="/img/star.png"
+              ref={imageRef}
+            ></img>
+          </div>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Intro;
